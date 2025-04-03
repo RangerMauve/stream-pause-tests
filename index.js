a526f5afa35629e76af7430a1f98d303ebfb4930
Mauve Signweaver <contact@mauve.moe>
2025-04-03 19:12:02 -0400

    Example with stream close before drain

--- index.js
+++ index.js
@@ -1,47 +1,86 @@
-import { pipelinePromise, Readable, Transform, Writable } from 'streamx'
+import { pipelinePromise, Transform, Writable } from 'streamx'
 import pDefer from 'p-defer'
 
-const controller = new AbortController()
+/*
+Goal:
+Simulate data being dropped before drain
 
-const source = Readable.from([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], {
-  signal: controller.signal
-})
+Looks like:
+- Write to stream, see that it's not drained, wait for first drain
+- Stream closes before drain
+- Stream should emit close but never emit drain
 
-const delayer = makeDelayer(500)
+*/
 
-const pausable = makePauseable()
+const pauseable = makePauseable()
 
-setInterval(pausable.toggle, 1000)
+const onDone = pipelinePromise(pauseable.stream, new Writable({
+  write (chunk, cb) {
+    console.log(`Log ${chunk}`)
+    cb()
+  }
+}))
+
+pauseable.stream.on('drain', () => console.log('drain'))
+pauseable.stream.on('close', () => console.log('close'))
+
+let drained = pauseable.stream.write('Hello')
+console.log(drained)
 
 setTimeout(() => {
-  console.log('Destroying source')
-  pausable.kill()
-  controller.abort()
-}, 4000)
+  pauseable.toggle()
+  drained = pauseable.stream.write('World')
+  console.log(drained)
 
-function makeDelayer (delayTime, signal) {
-  return new Transform({
-    signal,
-    transform (chunk, cb) {
-      setTimeout(() => {
-        this.push(chunk)
-        cb()
-      }, delayTime)
-    }
+  setTimeout(() => {
+    console.log('Destroying')
+    pauseable.kill()
   })
-}
+})
+
+await onDone.catch(() => console.log('It errored 🤷'))
 
 function makePauseable () {
   let onResume = null
   let killed = false
+  let hasListen = false
+
+  const stream = new Transform({
+    highWaterMark: 0,
+    transform (chunk, cb) {
+      if (killed) {
+        this.push(null)
+        cb(new Error('Killed'))
+        return
+      }
+      if (onResume) {
+        console.log(`Queue ${chunk}`)
+        hasListen = true
+        onResume.promise.then(() => {
+          console.log(`Flush ${chunk}`)
+          if (killed) {
+            this.push(null)
+            cb(new Error('Killed'))
+            return
+          }
+          this.push(chunk)
+          cb()
+        }).catch(cb)
+      } else {
+        this.push(chunk)
+        cb()
+      }
+    }
+  })
 
   function kill () {
     killed = true
-    if (onResume) {
-      onResume.reject(new Error('Killed while in progress'))
-    }
+    if (onResume && hasListen) {
+      onResume.reject(new Error('killed before flush'))
+    } else stream.destroy()
   }
   function toggle () {
+    hasListen = false
     if (!onResume) {
       // Pause
       onResume = pDefer()
@@ -52,49 +91,5 @@ function makePauseable () {
     }
   }
 
-  const stream = new Transform({
-    transform (chunk, cb) {
-      if (killed) {
-        this.push(null)
-        cb()
-        return
-      }
-      if (onResume) {
-        console.log(`Queue ${chunk}`)
-        onResume.promise.then(() => {
-          console.log(`Flush ${chunk}`)
-          if (killed) {
-            this.push(null)
-            cb()
-            return
-          }
-          this.push(chunk)
-          cb()
-        }, () => {
-          cb()
-        })
-      } else {
-        this.push(chunk)
-        cb()
-      }
-    }
-  })
-
   return { stream, toggle, kill }
 }
-
-await pipelinePromise(
-  source,
-  delayer,
-  pausable.stream,
-  new Writable({
-    write (chunk, cb) {
-      console.log(`Log ${chunk}`)
-      cb()
-    }
-  })
-)
-
-console.log('Done!')
-
-process.exit(0)
